b5ff0d0947c2df6292c42f5abf4a0ae2add21901
TheOnlySmartBoy <o2jose43@gmail.com>
2018-12-11 18:24:43 +0300

    Creating a java application to print hello world on the console

---
+++ src/BasicJava/HelloWorld/Helloworld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BasicJava.HelloWorld;
+
+/**
+ *
+ * @author Theonlysmartboy <Theonlysmartboy@github.com>
+ */
+public class Helloworld {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+    }
+}
